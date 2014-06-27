14eafee55cdfbf5b8b13d409bf18820a06bc16b1
boretti <mathieu.boretti@gmail.com>
2014-06-27 21:34:53 +0200

    issue #6 error in the javadoc

--- powerunit/src/main/java/org/powerunit/TestSuite.java
+++ powerunit/src/main/java/org/powerunit/TestSuite.java
@@ -142,7 +142,7 @@ public interface TestSuite extends Assert, Assume, Matchers {
 	 * <pre>
 	 * private TemporaryFolder temporary = temporaryFolder();
 	 * 
-	 * @#64;
+	 * &#64;
 	 * public TestRule rule = before(this::beforeMethodName).around(temporary);
 	 * </pre>
 	 * 
