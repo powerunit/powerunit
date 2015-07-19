32f25cd18fee35640628ef28620df0a62d4a2063
boretti <mathieu.boretti@gmail.com>
2015-07-19 13:36:14 +0200

    javadoc

--- powerunit/src/main/java/ch/powerunit/collector/lang/package-info.java
+++ powerunit/src/main/java/ch/powerunit/collector/lang/package-info.java
@@ -17,8 +17,8 @@
  * You should have received a copy of the GNU General Public License
  * along with Powerunit. If not, see <http://www.gnu.org/licenses/>.
  */
-/**{@link java.util.stream.Collector}.
- * DSL support for tester of 
+/**
+ * DSL support for tester of {@link java.util.stream.Collector}.
  * @author borettim
  *
  */
